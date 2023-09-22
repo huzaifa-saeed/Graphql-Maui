351c3fa44f46084f8dd8406aa1b69b9b1f7ed343
huzaifa saeed <huzaifa.saeed@breakthru.pk>
2023-09-22 20:43:03 +0500

    Graphql Implementation

---
+++ Graphql-Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Graphql_Maui;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ Graphql-Maui/MauiProgram.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using Graphql_Maui.Data;
+
+namespace Graphql_Maui;
+
+public static class MauiProgram
+{
+    public static string GraphQLUrl = "api-crypto-workshop.chillicream.com/graphql";
+    public static string baseHttp = "https://";
+    public static string baseWss = "wss://";
+
+    public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();
+#endif
+
+		builder.Services.AddSingleton<WeatherForecastService>();
+
+        builder.Services
+    .AddMauiGraphqlAppClient()
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri($"{baseHttp}{GraphQLUrl}"))
+    .ConfigureWebSocketClient(c => c.Uri = new Uri($"{baseWss}{GraphQLUrl}"));
+
+        return builder.Build();
+	}
+}
+
